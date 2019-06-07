261aceda733091d29ecd47c6abbbc7d9a855a64c
Michael Mottola <mottola64@gmail.com>
2019-06-07 02:34:20 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,38 +1,37 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-require 'pry'
 
 class InteractiveRecord
-
   def self.table_name
-    self.to_s.downcase.pluralize
+    "#{self.to_s.downcase}s"
   end
 
   def self.column_names
-    DB[:conn].results_as_hash = true
 
-    sql = "PRAGMA table_info('#{table_name}')"
+    sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
-    table_info.each do |column|
-      column_names << column["name"]
+    table_info.each do |row|
+      column_names << row["name"]
     end
     column_names.compact
   end
 
   def initialize(options={})
-    options.each do |property,value|
+    options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
 
-  def table_name_for_insert
-    self.class.table_name
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
-  def col_names_for_insert
-    self.class.column_names
+  def table_name_for_insert
+    self.class.table_name
   end
 
   def values_for_insert
@@ -43,14 +42,12 @@ class InteractiveRecord
     values.join(", ")
   end
 
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name='#{name}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
 
@@ -60,4 +57,3 @@ class InteractiveRecord
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
     DB[:conn].execute(sql)
   end
-end
